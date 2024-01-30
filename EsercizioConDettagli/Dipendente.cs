a73c4e3ead300a8ef6e560fc7247b4549aec9bf4
Mariline Luccisano <y.marilineluccisano@outlook.it>
2024-01-30 16:50:01 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioConDettagli/Dipendente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioConDettagli
+{
+    internal class Dipendente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Età { get; set; }
+
+
+
+        public void GetName()
+        {
+           
+            Nome = Console.ReadLine();
+        }
+
+        public void GetCognome()
+        {
+         
+            Cognome = Console.ReadLine();
+        }
+
+        public void GetEtà()
+        {
+          
+            Età = Convert.ToInt32(Console.ReadLine());
+        }
+
+      public void GetDettagli()
+        {
+            Console.WriteLine(Nome + " "+ Cognome + " "+ Età );
+
+        }
+
+    }
+}
---
+++ EsercizioConDettagli/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioConDettagli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dipendente dipendente1 = new Dipendente();
+
+            Console.WriteLine("inserisci il nome");
+            dipendente1.GetName();
+
+            Console.WriteLine("inserisci il cognome");
+            dipendente1.GetCognome();
+
+            Console.WriteLine("inserisci il Età");
+            dipendente1.GetEtà();
+           
+
+            dipendente1.GetDettagli();
+
+            Console.ReadLine();
+
+        }
+    }
+}
